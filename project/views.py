d3674e10ec6eee2b30ac8a7cce3d39c204112353
David García Alonso <g.a.david@gmail.com>
2017-09-14 13:40:43 +0200

    Flask: FlaskTaskr, Part 3 - Error Handling and Testing p.164

--- project/views.py
+++ project/views.py
@@ -2,7 +2,7 @@
 
 from functools import wraps
 
-from flask import Flask, flash, redirect, render_template, url_for, request, session, g
+from flask import Flask, flash, redirect, render_template, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
 from forms import AddTaskForm, RegisterForm, LoginForm
 
@@ -33,6 +33,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('Goodbye!!')
     return redirect(url_for('login'))
 
@@ -47,6 +48,7 @@ def login():
             user = User.query.filter_by(name=request.form['name']).first()
             if user is not None and user.password == request.form['password']:
                 session['logged_in'] = True
+                session['user_id'] = user.id
                 flash('Welcome!!')
                 return redirect(url_for('tasks'))
             else:
@@ -78,18 +80,22 @@ def new_task():
     form = AddTaskForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
-            new_task = Task(
+            a_new_task = Task(
                 form.name.data,
                 form.due_date.data,
                 form.priority.data,
                 datetime.datetime.utcnow(),
                 '1',
-                '1'
+                session['user_id']
             )
-            db.session.add(new_task)
+            db.session.add(a_new_task)
             db.session.commit()
             flash('New entry was succesfully posted. Thanks')
-    return redirect(url_for('tasks'))
+            return redirect(url_for('tasks'))
+        else:
+            flash('All fileds are required.')
+            return redirect(url_for('tasks'))
+    return render_template('tasks.html', form=form)
 
 
 # Marcar tarea como completada
@@ -133,4 +139,3 @@ def register():
             return redirect(url_for('login'))
 
     return render_template('register.html', form=form, error=error)
-
