d1c4c839ad2dc22889f2e2a31967a05c61598e7a
David García Alonso <g.a.david@gmail.com>
2017-09-15 15:16:24 +0200

    Testing p.178

---
+++ test.py
@@ -0,0 +1,48 @@
+# project/test.py
+
+
+import os
+import unittest
+
+
+from views import app, db
+from _config import basedir
+from models import User
+
+
+TEST_DB = 'test.db'
+
+
+class AllTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
+
+    # execute after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    # each test should start with test
+    def test_user_can_register(self):
+        new_user = User("michael", "michael@mherman.org", "michaelherman")
+        db.session.add(new_user)
+        db.session.commit()
+        test = db.session.query(User).all()
+        for t in test:
+            t.name
+
+        assert t.name == "michael"
+
+
+if __name__ == '__main__':
+    unittest.main()
