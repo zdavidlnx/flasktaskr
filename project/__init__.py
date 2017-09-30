d313d8331c4a585829a9e46a7e552077f8d27026
David García Alonso <g.a.david@gmail.com>
2017-09-30 14:41:11 +0200

    Template. p. 245

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 # project/__init__.py
 
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -17,3 +17,8 @@ from project.tasks.views import tasks_blueprint
 # register our blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', 404)
