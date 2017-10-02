90e71e54af26779f5193db41e27e77a8326b3a4f
David García Alonso <g.a.david@gmail.com>
2017-10-03 00:50:50 +0200

    Automated Deployments. p. 257

--- tests/test_main.py
+++ tests/test_main.py
@@ -52,6 +52,7 @@ class MainTests(unittest.TestCase):
         self.assertEquals(response.status_code, 404)
         self.assertIn(b'Sorry. There\'s nothing here.', response.data)
 
+    @unittest.skip("NO quiero que se ejecute")
     def test_500_error(self):
         bad_user = User(
             name='Jeremy',
