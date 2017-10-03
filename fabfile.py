c2ec4ec333ab1b32e55073184eb1c03452aeaddb
David García Alonso <g.a.david@gmail.com>
2017-10-03 14:55:06 +0200

    Prueba Fabric3

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+
+# prep
+def test():
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm('Test failed. Continue?'):
+        abort("Aborted at user promp")
+
+
+def commit():
+    message = input("Enter a git commit message: ")
+    local("git add .")
+    local("git commit -am '{}'".format(message))
+
+
+def push():
+    local("git push origin master")
+
+
+def prepare():
+    test()
+    commit()
+    push()
