65d297e9fe6af1a53fb771eb28622b76a99210d4
David García Alonso <g.a.david@gmail.com>
2017-10-03 15:05:36 +0200

    from fabric.api import local, settings, abort

--- fabfile.py
+++ fabfile.py
@@ -3,17 +3,17 @@ from fabric.contrib.console import confirm
 
 
 # prep
+
 def test():
     with settings(warn_only=True):
         result = local("nosetests -v", capture=True)
-    if result.failed and not confirm('Test failed. Continue?'):
-        abort("Aborted at user promp")
+    if result.failed and not confirm("Tests failed. Continue?"):
+        abort("Aborted at user request.")
 
 
 def commit():
-    message = input("Enter a git commit message: ")
-    local("git add .")
-    local('git commit -am "{}"'.format(message))
+    message = raw_input("Enter a git commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
 
 
 def push():
@@ -24,3 +24,31 @@ def prepare():
     test()
     commit()
     push()
+
+
+# deploy
+
+def pull():
+    local("git pull origin master")
+
+
+def heroku():
+    local("git push heroku master")
+
+
+def heroku_test():
+    local("heroku run nosetests -v")
+
+
+def deploy():
+    pull()
+    test()
+    commit()
+    heroku()
+    heroku_test()
+
+
+# rollback
+
+def rollback():
+    local("heroku rollback")
